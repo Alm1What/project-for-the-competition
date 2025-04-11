2f2ea80b7a889a7582b6b5d614ec0d2f6c542399
evgen <zekanov16@gmail.com>
2025-04-11 23:36:44 +0300

    перший коміт

---
+++ src/main/java/org/example/projectforthecompetition/ProjectForTheCompetitionApplication.java
@@ -0,0 +1,13 @@
+package org.example.projectforthecompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForTheCompetitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectForTheCompetitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projectforthecompetition/ProjectForTheCompetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectforthecompetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForTheCompetitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
